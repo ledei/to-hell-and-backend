6fc2cc7025cc8ccd1bc532c5459f9c2df9d234ba
ledei <holybuzz@live.se>
2023-04-05 14:05:46 +0200

    made function to generate token and verify, and a middleware to check for valid tokens

---
+++ server/express-server/src/middleware/jwtChecker.js
@@ -0,0 +1,21 @@
+import jwtUtils from "../util/jwtUtil.js";
+
+function authorization(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  if (authHeader == undefined) {
+    res.status(400).send("Authorization header is missing");
+  } else {
+    const token = authHeader.replace("Bearer ", "");
+    try {
+      const payload = jwtUtils.verify(token);
+      req.user = payload;
+      next();
+    } catch (err) {
+      console.log(err);
+      console.log(req.ip, err.serverMessage);
+
+      res.status(403).send(err.clientMessage);
+    }
+  }
+}
+export default { authorization };
