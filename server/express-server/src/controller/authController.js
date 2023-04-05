31419a3ed13f4e88940f6241e8d1d0f687abed6a
ledei <holybuzz@live.se>
2023-04-05 14:40:23 +0200

    made an endpoint to register a new user

--- server/express-server/src/controller/authController.js
+++ server/express-server/src/controller/authController.js
@@ -1,4 +1,4 @@
-import { findUser } from "../service/chatAppService.js";
+import { createUser, findUser } from "../service/chatAppService.js";
 import jwtUtils from "../util/jwtUtil.js";
 
 async function login(req, res) {
@@ -23,4 +23,34 @@ async function login(req, res) {
   }
 }
 
-export default { login };
+async function registerUser(req, res) {
+  let UserDetails = {
+    username: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    password: req.body.password,
+    role: "user",
+  };
+  if (UserDetails.email == "" || UserDetails.password == "") {
+    res.status(400);
+    res.send("Missing user details");
+  } else {
+    const registered = await findUser(UserDetails.username);
+
+    if (registered == null) {
+      const newUser = await createUser(UserDetails);
+      if (newUser.acknowledged == true) {
+        const token = jwtUtils.generate(UserDetails);
+        res.status(201);
+        res.send(token);
+      } else {
+        res.send("error");
+      }
+    } else {
+      res.status(400);
+      res.send("This user already exist");
+    }
+  }
+}
+
+export default { login, registerUser };
