642fe1613a4163cee027181f6576475be3ca48d1
ledei <holybuzz@live.se>
2023-04-05 14:43:23 +0200

    added the endpoints to authrouter

--- server/express-server/src/router/authRouter.js
+++ server/express-server/src/router/authRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
+import authController from "../controller/authController.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/auth/create");
+authRouter.post("/auth/register", authController.registerUser);
 authRouter.post("/auth/logout");
-authRouter.post("/auth/login");
+authRouter.post("/auth/login", authController.login);
 
 export default authRouter;
