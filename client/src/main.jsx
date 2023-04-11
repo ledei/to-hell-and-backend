02f05d8ee512f194b307f5fe210a0a11d954db11
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2023-04-11 14:13:42 +0200

    started channel-page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
-import LoginPage from './pages/Login'
+import LoginPage from './pages/LoginPage'
+import ChannelPage from './pages/ChannelPage';
 import './stylesheet/main.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <LoginPage />
+    <ChannelPage />
   </React.StrictMode>
 )
